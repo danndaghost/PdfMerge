ca0fa48f9bc963d0541a21fe5572ef52a09a4a58
Daniel Araneda <daniel.araneda.h@gmail.com>
2024-08-05 20:37:19 -0400

    Se cambia estructura para recorrer folders

--- run.py
+++ run.py
@@ -1,10 +1,15 @@
 import os
+from openpyxl import load_workbook
 from fnmatch import fnmatch
 from PyPDF2 import PdfReader, PdfWriter
 
-root = './'
+rootpath = './'
 pattern = "*.pdf"
-pdffiles = [];
+#rootpath = r'\\192.168.0.11\cormup\Dir_Personas\Contratos-General'
+writepath = "Contratos SLEP"
+readpath = ['PC Bastian', 'Publicacion']
+pdffiles = []
+dotacion = []
 
 def merge_pdfs(paths, output):
     pdf_writer = PdfWriter()
@@ -19,18 +24,30 @@ def merge_pdfs(paths, output):
     with open(output, 'wb') as out:
         pdf_writer.write(out)
 
-def main():
-    for rut in os.listdir(root):
-        if(os.path.isdir(rut)):
-            for path, subdirs, files in os.walk(rut):
+def leerExcel():
+    filepath        = rootpath
+    archivo         = filepath + 'dotacion'    
+    archivo_excel   = archivo + '.xlsx'
+    workbook = load_workbook(archivo_excel)
+    hoja = workbook.worksheets[0]
+    for row in range(2, hoja.max_row+1): #start on two to ignore the title of column
+        dotacion.append(hoja.cell(row=row, column=1).value)
+    return dotacion
+
+def crearPdf(readpath, dotacion):
+    for rut in dotacion:
+        for rootfolder in readpath:
+            for path, subdirs, files in os.walk(rootpath + '/' + rootfolder):
                 for name in files:
-                    if fnmatch(name, pattern):
+                    if fnmatch(name, pattern) and (rut in name):
                         pdffiles.append(os.path.join(path, name))
-                #print(path)
-            merge_pdfs(pdffiles, output=rut + '/' + rut + '.penalolen.pdf')    
-            pdffiles.clear()
+        
+        if len(pdffiles) > 0:
+            merge_pdfs(pdffiles, output=rootpath + '/' + writepath + '/' + rut + '.penalolen.pdf')    
+        pdffiles.clear()
 
         
 if __name__ == "__main__":
-    main()
+    dotacion = leerExcel() #obtener los rut a buscar en las carpetas
+    crearPdf(readpath, dotacion)
    
